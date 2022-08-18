f9748b0851df908459078e585de75af6f0c3365e
rafpank <portaleobs@gmail.com>
2022-08-18 18:48:48 +0200

    Nauka zmiennych w Java

--- Main.java
+++ Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej Rafal");
-        System.out.println("SUPER GRUPA");
+        String name = "Rafal";
+        int age = 98;
+        char znak = 'a';
+        int x = 10;
+        int y = 20;
+        int c = x + y;
+        System.out.println("wynik dodawania " +x+" + " + y+ " to "+ c);
+        //System.out.println("Hej "+name+ " ktory masz "+age+" lat");
+
+
+
+
     }
 }
