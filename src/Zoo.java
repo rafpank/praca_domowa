12273a5af5ed9a60f18f8d5c4c2a2f491658c8a5
rafpank <portaleobs@gmail.com>
2022-08-18 20:53:38 +0200

    Utworzenie klasy "Zoo", a w niej pętla w która wywyołuje nazwy zwierząt z tabeli

---
+++ src/Zoo.java
@@ -0,0 +1,15 @@
+public class Zoo {
+    public static void main(String[] args) {
+        String [] animals = new String[4];
+        animals = new String[]{"Lewy", "Pingwiny", "Niedzwiedzie", "Malpy", "Pelikany"};
+
+//                for (int i=0; i< animal.length; i++) {
+//                    System.out.println(animal[i]);
+//                }
+        for (String animal:animals)
+        {
+            System.out.println(animal);
+        }
+    }
+
+}
